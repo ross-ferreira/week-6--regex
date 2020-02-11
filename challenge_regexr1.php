aab31fcc89539b33d8404e621ddaf1de230593a4
Ross Ferreira <ross.p.ferreira@gmail.com>
2020-02-11 12:42:42 +0000

    added challenge_regexr1.php

---
+++ challenge_regexr1.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+function hasNumber(string $str) : bool {
+    return preg_match("/\d/", $str) === 1;
+}
+
+var_dump(hasNumber("blah")); // bool(false)
+var_dump(hasNumber("bl3h")); // bool(true)
+var_dump(hasNumber("bl3h5")); // bool(true)
+
+// Create a function hasNumber, that returns true if the given string contains a digit:
